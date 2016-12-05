535d96e9099774aade5e87ad888ea124640decf5
Shuyang Shi <shuyang790@gmail.com>
2016-12-05 20:29:01 +0800

    Adding feature: user resume delivery

---
+++ src/main/scala/FeatureExtractorUserDelivery.scala
@@ -0,0 +1,60 @@
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by Ivan on 2016/12/3.
+ */
+object FeatureExtractorUserDelivery {
+
+  /*
+    [Table] 58data_delivery
+   */
+  def createTableDelivery(sc: SparkContext,
+                          sqlContext: org.apache.spark.sql.SQLContext
+                           ) = {
+
+    val textFiles = sc.textFile("hdfs:///zp/58Data/delivery/delivery_*")
+
+    val schemaString = "cookieid infoid entuserid resumeid resumeuserid deliverytime slot"
+    val dataStructure = new StructType(
+        schemaString.split(" ").map(fieldName =>
+          StructField(fieldName, StringType, nullable = false)
+        )
+    )
+
+    val rowRDD = textFiles
+      .map(_.split("\001"))
+      .filter(xs => xs.length >= 7)
+      .map(xs => Row(xs(0), xs(1), xs(2), xs(3), xs(4), xs(5), xs(6)))
+
+    val namedDF = sqlContext.createDataFrame(
+      rowRDD,
+      dataStructure
+    )
+
+    namedDF.registerTempTable("58data_delivery")
+  }
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkFeatureExtractor")
+    val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    createTableDelivery(sc, sqlContext)
+
+    val results = sqlContext.sql(
+      """
+        SELECT
+          resumeuserid AS userid,
+          resumeid,
+          infoid AS positionid,
+          FROM_UNIXTIME(deliverytime,'YYYYMMdd') AS deliverydate
+        FROM 58data_delivery
+        WHERE resumeuserid <> '-'
+      """.stripMargin)
+
+    results.write.mode("overwrite").save("hdfs:///user/shuyangshi/58feature_userdeliveries")
+  }
+
+}
