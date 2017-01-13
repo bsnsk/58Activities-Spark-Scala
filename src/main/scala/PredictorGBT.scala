bd7aa6f9242aa7d3a6332e89729dfd19a0f73458
Shuyang Shi <shuyang790@gmail.com>
2017-01-13 17:13:21 +0800

    More working Predictors: GBT, RF, DT
    There may be some more space for modifications on the parameters, but
    right now they can basically work.
    
    RF Result (about 0.75 AUC):
    
    ```
    (20161005,(0.7436755620357189,(0.07782555878372449,0.9099697558868006,0.14338781258909084,(16849,199648,272759,1667))))
    (20161006,(0.745376523302378,(0.07710343734224481,0.8937950379648886,0.1419606095987218,(18481,221210,327641,2196))))
    (20161007,(0.7465484238876854,(0.07833741753063148,0.8967288106335232,0.14408748292432616,(20779,244471,361207,2393))))
    (20161008,(0.7564234526112977,(0.0664146546530642,0.8987636624260885,0.1236892234284552,(20064,282038,448788,2260))))
    (20161009,(0.7608616855576478,(0.04488746898927341,0.8957828750534265,0.08549100389893366,(12575,267570,447744,1463))))
    ```
    
    GBT Result (about 0.5+ AUC):
    
    ```
    (20161005,(0.5470521823303223,(0.8777555110220441,0.09462086843810759,0.17082683307332294,(1752,244,472163,16764))))
    (20161006,(0.5491521946297542,(0.8813632441760139,0.09880543599168158,0.17769080234833662,(2043,275,548576,18634))))
    (20161007,(0.5502186113534697,(0.8846444780635401,0.10094079060935612,0.18120545398202667,(2339,305,605373,20833))))
    (20161008,(0.5636598443352666,(0.8641452344931921,0.12793406199605806,0.22287252721526396,(2856,449,730377,19468))))
    (20161009,(0.5001068528280381,(1.0,2.1370565607636415E-4,4.273199914536002E-4,(3,0,715314,14035))))
    ```
    
    DT Result (about 0.5+ AUC):
    ```
    (20161005,(0.5471617704010819,(0.8899594320486816,0.09478289047310434,0.1713197969543147,(1755,217,472190,16761))))
    (20161006,(0.5487387922493093,(0.8897188049209139,0.09793490351598394,0.17644752319958174,(2025,251,548600,18652))))
    (20161007,(0.5497760958204463,(0.8970189701897019,0.09999136889349215,0.17992622791690935,(2317,266,605412,20855))))
    (20161008,(0.5638330460510613,(0.870742092457421,0.12824762587349936,0.22356707793221925,(2863,425,730401,19461))))
    (20161009,(0.5000712352186921,(1.0,1.4247043738424277E-4,2.849002849002849E-4,(2,0,715314,14036))))
    ```

---
+++ src/main/scala/PredictorGBT.scala
@@ -0,0 +1,42 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.GBTClassifier
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.GradientBoostedTrees
+import org.apache.spark.mllib.tree.configuration.BoostingStrategy
+import org.apache.spark.rdd.RDD
+
+/**
+  * Created by Ivan on 2017/1/7.
+  */
+object PredictorGBT extends PredictionTest {
+
+  def predictionResultLabelsAndScores(
+                                       trainingData: RDD[(Double, Vector)],
+                                       testData: RDD[(Int, (Double, Vector))],
+                                       sqlContext: org.apache.spark.sql.SQLContext
+                                     ): RDD[(Int, (Int, Int))] = {
+    // Train a GradientBoostedTrees model.
+    // The defaultParams for Classification use LogLoss by default.
+
+    var boostingStrategy = BoostingStrategy.defaultParams("Classification")
+    boostingStrategy.numIterations = 5 // Note: Use more iterations in practice.
+    boostingStrategy.treeStrategy.numClasses = 2
+    boostingStrategy.treeStrategy.maxDepth = 6
+
+    // Empty categoricalFeaturesInfo indicates all features are continuous.
+    boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
+
+    val model = GradientBoostedTrees.train(
+      trainingData.map(xs => LabeledPoint(xs._1, xs._2)),
+      boostingStrategy
+    )
+
+    testData
+      .map(data => {
+        val prediction = model.predict(data._2._2)
+        (data._1, (data._2._1.toInt, prediction.toInt))
+      })
+  }
+}
---
+++ src/main/scala/PredictorRF.scala
@@ -0,0 +1,41 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.rdd.RDD
+
+/**
+  * Created by Ivan on 2017/1/7.
+  */
+object PredictorRF extends PredictionTest {
+
+  def predictionResultLabelsAndScores(
+                                       trainingData: RDD[(Double, Vector)],
+                                       testData: RDD[(Int, (Double, Vector))],
+                                       sqlContext: org.apache.spark.sql.SQLContext
+                                     ): RDD[(Int, (Int, Int))] = {
+
+    // Train a RandomForest model.
+    // Empty categoricalFeaturesInfo indicates all features are continuous.
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 10 // Use more in practice.
+    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    val impurity = "gini"
+    val maxDepth = 4
+    val maxBins = 32
+
+    val model = RandomForest.trainClassifier(
+      trainingData.map(xs => LabeledPoint(xs._1, xs._2)),
+      numClasses, categoricalFeaturesInfo, numTrees,
+      featureSubsetStrategy, impurity, maxDepth, maxBins)
+
+    testData
+      .map(data => {
+        val prediction = model.predict(data._2._2)
+        (data._1, (data._2._1.toInt, prediction.toInt))
+      })
+  }
+}
