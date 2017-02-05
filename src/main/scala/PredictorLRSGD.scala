c56d740a02e4a29cc0d787998e2de54f805d96cf
Shuyang Shi <shuyang790@gmail.com>
2017-02-05 18:06:49 +0800

    Add predictor: LR using SGD
    For future modification on the formula.
    
    To achieve a result like before, positive / negative samples should be
    balanced.
    
    ```
    $ cat 20170205175332LRSGD/part-00000
    (20161005,(0.7455367737301328,(0.07737105202333096,0.9220133938215598,0.14276217036631308,(17072,203579,268828,1444))))
    (20161006,(0.7475918655420719,(0.07658893461362597,0.9072882913382019,0.14125389182331216,(18760,226184,322667,1917))))
    (20161007,(0.7486881985816703,(0.07779474581147777,0.9101501812532367,0.1433377510449587,(21090,250008,355670,2082))))
    (20161008,(0.7585711389560715,(0.06588500213534185,0.9122021143164307,0.12289383478974557,(20364,288720,442106,1960))))
    (20161009,(0.762682408069967,(0.04443639605505098,0.9089613905114688,0.08473056874398219,(12760,274392,440922,1278))))
    ```

---
+++ src/main/scala/PredictorLRSGD.scala
@@ -0,0 +1,76 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.{DenseVector, Vector, Vectors}
+import org.apache.spark.mllib.optimization.{Gradient, GradientDescent, SquaredL2Updater}
+import org.apache.spark.mllib.util.MLUtils
+
+import scala.math
+
+/**
+  * Created by Ivan on 2017/1/16.
+  */
+object PredictorLRSGD extends PredictionTest {
+
+  override var identifier: String = "LRSGD"
+
+  class myGradient extends Gradient {
+    override def compute(data: Vector, label: Double, weights: Vector, cumGradient: Vector): Double = {
+
+      val margin = -1.0 * (data.toArray, weights.toArray).zipped.map(_*_).sum
+      val multiplier = (1.0 / (1.0 + math.exp(margin))) - label
+      //y += a * x，即cumGradient += multiplier * data
+      var accumulativeGradient = cumGradient.toDense.values
+      data.foreachActive((i, feature) => {
+        accumulativeGradient(i) += multiplier * feature
+      })
+      if (label > 0.5) {
+        math.log(1 + math.exp(margin))
+      } else {
+        math.log(1 + math.exp(margin)) - margin
+      }
+
+    }
+  }
+
+  def predictionResultLabelsAndScores(
+                                       trainingData: RDD[(Double, Vector)],
+                                       testData: RDD[(Int, (Double, Vector))],
+                                       sqlContext: org.apache.spark.sql.SQLContext
+                                     ): RDD[(Int, (Int, Int))] = {
+    val convergenceTol = 1e-4
+    val numIterations = 10
+    val regParam = 0.01
+    val initialWeights = Vectors.dense(Array.fill(trainingData.first()._2.size)(0.0))
+    val gradient = new myGradient()
+
+    val cntPositiveSamples = trainingData.filter(r => r._1 == 1).count()
+    val cntNegativeSamples = trainingData.filter(r => r._1 == 0).count()
+    val rate = (cntNegativeSamples.toDouble / cntPositiveSamples.toDouble).toInt
+
+    val weights = GradientDescent
+      .runMiniBatchSGD(
+        trainingData.flatMap(xs =>
+          if (xs._1.toInt == 1) List.fill(rate)(xs)
+          else List(xs)
+        ),
+        gradient,
+        new SquaredL2Updater(),
+        stepSize = 0.1,
+        numIterations,
+        regParam,
+        miniBatchFraction = 1.0,
+        initialWeights,
+        convergenceTol
+      )._1
+
+    val calcScore: Vector => Double = features => 1.0 / (1.0 + scala.math.exp(
+      - (weights.toArray, features.toArray).zipped.map(_*_).sum
+    ))
+
+    testData
+      .map(data => {
+        val prediction = if (calcScore(data._2._2) > 0.5) 1 else 0
+        (data._1, (data._2._1.toInt, prediction.toInt))
+      })
+
+  }
+}
